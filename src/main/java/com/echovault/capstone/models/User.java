43c092fc7187b580e0b9034df8330de3e5fc39ec
William Tisdale <will.tisdale.31@gmail.com>
2021-03-09 11:55:12 -0600

    added user model

---
+++ src/main/java/com/echovault/capstone/models/User.java
@@ -0,0 +1,121 @@
+package com.echovault.capstone.models;
+
+import jdk.jfr.Unsigned;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Unsigned
+    private long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column
+    private String image;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Memory> memories;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Echo> echoes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Comment> comments;
+
+    public User(){}
+
+    public User(String email, String password, String firstName, String lastName){
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<Memory> getMemories() {
+        return memories;
+    }
+
+    public void setMemories(List<Memory> memories) {
+        this.memories = memories;
+    }
+
+    public List<Echo> getEchoes() {
+        return echoes;
+    }
+
+    public void setEchoes(List<Echo> echoes) {
+        this.echoes = echoes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
