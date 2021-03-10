f0ffacd99ca8b9d9b32c9da57d61cd642bada7d5
William Tisdale <will.tisdale.31@gmail.com>
2021-03-10 11:16:28 -0600

    completed security without testing and created login.html

--- src/main/java/com/echovault/capstone/services/UserDetailsLoader.java
+++ src/main/java/com/echovault/capstone/services/UserDetailsLoader.java
@@ -6,7 +6,9 @@ import com.echovault.capstone.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsLoader implements UserDetailsService {
 
     private final UserRepository usersDao;
