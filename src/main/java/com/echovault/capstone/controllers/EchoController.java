b3fe4891e256f0a96ab6a8fdb6062941c56bd2a9
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-03-11 17:00:44 -0600

    building out echo-vault functionality

--- src/main/java/com/echovault/capstone/controllers/EchoController.java
+++ src/main/java/com/echovault/capstone/controllers/EchoController.java
@@ -1,12 +1,20 @@
 package com.echovault.capstone.controllers;
 
 
+import com.echovault.capstone.Util.FileUpload;
 import com.echovault.capstone.models.Echo;
+import com.echovault.capstone.models.User;
 import com.echovault.capstone.repositories.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @Controller
 public class EchoController {
@@ -26,8 +34,40 @@ public class EchoController {
         this.linkDao = linkDao;
         this.userDao = userDao;
     }
+    @Value("${file-upload-path}")
+    private String uploadPath;
 
+    @GetMapping("/echo-create")
+    public String showCreateForm() {
+        return "echo-create";
+    }
 
+    @PostMapping("/echo-create")
+    public String saveFile(@ModelAttribute Echo echo,
+                           @RequestParam(name = "profile-img") MultipartFile uploadedFile,
+                           @RequestParam(name = "background-img") MultipartFile uploadedFile2,
+                           @RequestParam(name = "carousel-img") MultipartFile uploadedFile3,
+                           Model model
 
+    ) {
+        FileUpload.savedFile(uploadedFile, echo, uploadPath);
+        FileUpload.savedFile(uploadedFile2, echo, uploadPath);
+        FileUpload.savedFile(uploadedFile3, echo, uploadPath);
 
+        echo.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        Echo savedEcho = echoDao.save(echo);
+        String subject = "New Post Created!";
+//        String body = "Dear " + savedPost.getUser().getUsername() + ". Thank you for creating a post. Your post id is: " + savedPost.getId();
+//
+//        emailService.prepareAndSend(savedEcho, subject, body);
+        model.addAttribute("message", "File successfully uploaded!");
+        return "redirect:/echo-create";
+
+    }
 }
+
+
+
+
+
+
