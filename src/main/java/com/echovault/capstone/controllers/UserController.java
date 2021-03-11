eef7e7d03fa9179970b46c037e66c7321269f858
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-03-11 11:48:38 -0600

    working on user controller to view profile page

---
+++ src/main/java/com/echovault/capstone/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.echovault.capstone.controllers;
+
+import com.echovault.capstone.models.User;
+import com.echovault.capstone.repositories.UserRepository;
+import com.echovault.capstone.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserController {
+
+    private final UserRepository userDao;
+    private final UserService userService;
+
+    public UserController(UserRepository userDao, UserService userService){
+        this.userDao = userDao;
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile/{id}")
+    public String showProfile(@PathVariable long id, Model model){
+        User user = userService.getLoggedInUser();
+        model.addAttribute("user", user);
+        return "/profile";
+
+    }
+}
