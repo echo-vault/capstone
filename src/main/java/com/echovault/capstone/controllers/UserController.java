6507b2a4284b828d71e1e7467052e5c181de215c
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-03-16 10:55:58 -0500

    worked on editing profile image, users can now view their created echoes on their profile page

--- src/main/java/com/echovault/capstone/controllers/UserController.java
+++ src/main/java/com/echovault/capstone/controllers/UserController.java
@@ -3,13 +3,16 @@ package com.echovault.capstone.controllers;
 import com.echovault.capstone.models.User;
 import com.echovault.capstone.repositories.UserRepository;
 import com.echovault.capstone.services.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @Controller
 public class UserController {
@@ -22,23 +25,43 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Value("${file-upload-path}")
+    private String uploadPath;
+
     @GetMapping("/profile")
     public String showProfile(Model model){
         User sessionUser = userService.getLoggedInUser();
         User user = userDao.getOne(sessionUser.getId());
+        model.addAttribute("echoes", user.getEchoes());
         model.addAttribute("user", user);
         return "profile";
     }
 
     @GetMapping("/profile/edit")
     public String editProfile(Model model){
-        User user = userService.getLoggedInUser();
+        User sessionUser = userService.getLoggedInUser();
+        User user = userDao.getOne(sessionUser.getId());
         model.addAttribute("user", user);
         return "profile-edit";
     }
 
     @PostMapping("/profile/edit")
-    public String goToProfile(@ModelAttribute User user){
+    public String goToProfile(@ModelAttribute User user, @RequestParam(name = "user-profile-img-edit") MultipartFile uploadedFile, Model model){
+        if(uploadedFile != null) {
+            String fileName = uploadedFile.getOriginalFilename();
+            String filePath = Paths.get(uploadPath, fileName).toString();
+            File destinationFile = new File(filePath);
+            try {
+                uploadedFile.transferTo(destinationFile);
+                user.setImage("/uploads/" + fileName);
+                model.addAttribute("message", "File successfully uploaded!");
+            } catch (IOException e) {
+                e.printStackTrace();
+                model.addAttribute("message", "Oops! Something went wrong! " + e);
+            }
+        }else{
+            user.setImage(user.getImage());
+        }
         User sessionUser = userService.getLoggedInUser();
         User editedUser = userDao.getOne(sessionUser.getId());
         user.setId(editedUser.getId());
