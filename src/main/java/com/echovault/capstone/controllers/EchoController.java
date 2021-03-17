f805c0ad288b272a689aac65767368642cfa040e
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-03-17 11:55:35 -0500

    tried error message for unsuccessful delete, didn't work

--- src/main/java/com/echovault/capstone/controllers/EchoController.java
+++ src/main/java/com/echovault/capstone/controllers/EchoController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
@@ -255,7 +256,6 @@ public class EchoController {
         redirectAttributes.addFlashAttribute("success", "Echo successfully deleted.");
         return "redirect:/profile";
     }
-
 
     @PostMapping("/memory")
     public String createMemory(@ModelAttribute Memory memory,
